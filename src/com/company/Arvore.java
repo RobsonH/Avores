6cd388bddb8c903a8687e57f7dfee6f85109a108
Robson Hammer <robsonads531@gmail.com>
2018-12-06 20:07:12 -0400

    Percorrendo

--- src/com/company/Arvore.java
+++ src/com/company/Arvore.java
@@ -18,21 +18,68 @@ public class Arvore {
         if(valor < conteudo){
             if (esquerda == null){
                 esquerda= new Arvore(valor);
-                System.out.println(valor + " a esquerda de " + conteudo);
+                //System.out.println(valor + " a esquerda de " + conteudo);
             }else {
                 esquerda.inserir(valor);
             }
         }else if(valor > conteudo){//inserir na direita
             if (direita == null){
                 direita = new Arvore(valor);
-                System.out.println(valor + " a direita de " + conteudo);
+                //System.out.println(valor + " a direita de " + conteudo);
             }else {
                 direita.inserir(valor);
             }
 
         }
     }
-
+    public void preOrdem(){
+        //Visistar a Raiz
+        System.out.print(conteudo + " ");
+        //Percorrer a esquerda
+        if (esquerda != null){
+            esquerda.preOrdem();
+        }
+        //Percorrer a direita
+        if (direita != null){
+            direita.preOrdem();
+        }
+    }
+    public void inOrdem(){
+        //Percorre a esquerda
+        if (esquerda != null){
+            esquerda.inOrdem();
+        }
+        //Imprime a raiz
+        System.out.print(conteudo + " ");
+        //Percorre a direita
+        if (direita != null) {
+            direita.inOrdem();
+        };
+    }
+        public void posOrdem(){
+        //Percorre a esquerda
+        if (esquerda != null){
+            esquerda.posOrdem();
+        }
+        //Percorre a direita
+        if (direita != null){
+            direita.posOrdem();
+        }
+        //Imprime a raiz
+        System.out.print(conteudo + " ");
+    }
+    public void inOrdemIvertida(){
+        //Percorre a direita
+        if (direita != null){
+            direita.inOrdemIvertida();
+        }
+        //Imprime a raiz
+        System.out.print(conteudo + " ");
+        //Percorre a esquerda
+        if (esquerda!= null){
+            esquerda.inOrdemIvertida();
+        }
+    }
 
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,22 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-    Arvore arvore = new Arvore(10);
-    arvore.inserir(8);
-    arvore.inserir(12);
-    arvore.inserir(9);
-    arvore.inserir(16);
-    arvore.inserir(4);
-    arvore.inserir(13);
+    Arvore arvore = new Arvore(4);
+    arvore.inserir(2);
+    arvore.inserir(1);
+    arvore.inserir(3);
+    arvore.inserir(6);
+    arvore.inserir(7);
+    arvore.inserir(5);
+
+        System.out.println( "Pre-ordem");
+        arvore.preOrdem();
+        System.out.println("\n in ordem");
+        arvore.inOrdem();
+        System.out.println("\n pos ordem");
+        arvore.posOrdem();
+        System.out.println("\n ordem invertida");
+        arvore.inOrdemIvertida();
 
 
 
